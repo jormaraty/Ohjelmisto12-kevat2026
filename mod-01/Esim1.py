ea700b4812b58dbc3ff4cc425985751e4f1d8adc
ratyj <jorma.raty@metropolia.fi>
2026-01-12 20:07:10 +0200

    Initial commit

---
+++ mod-01/Esim1.py
@@ -0,0 +1,11 @@
+# perustulostuksia
+print("Hello World")
+print("Tämän koodasin ihan itse")
+
+# kysytään käyttältä tämän nimi ja
+# otetaan saatu vastaus talteen muuttujaan nimeltä name
+name = input("Anna nimesi: ")
+
+# tulostetaan käyttäjälle tervehdys, jossa on mukana käyttäjän nimi.
+# Huomaa: vakiotekstit "..." sisään, muuttujan nimi sellaisenaan.
+print("Moikka " + name + ", mitä kuuluu?")
