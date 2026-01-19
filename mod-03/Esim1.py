8b5a1f48e74d31e3c7336d73d0d6e256fe05130c
jormaraty <jorma.raty@metropolia.fi>
2026-01-19 10:16:10 +0200

    Uusi yritys

---
+++ mod-03/Esim1.py
@@ -0,0 +1,18 @@
+# kysytään käyttäjän ikä ja kysytään rahamäärää
+ika = int(input("Anna ikäsi: "))
+rahat = float(input("Paljonko on rahaa mukana: "))
+
+# jos käyttäjä on täysi-ikäinen ja rahaa on
+# vähintään 10€, niin pääsee bileisiin.
+# Muuten toivotetaan kotimatkaa.
+if ika >= 18  and rahat >= 10:
+    print("Hyvää iltaa,")
+    print("Tervetuloa bileisiin!!!")
+else:
+    print("Go home!")
+
+print("Ohjelma loppuu")
+
+
+
+
