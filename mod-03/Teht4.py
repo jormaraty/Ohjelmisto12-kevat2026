7de61596d3f23a7d02b0dd30e1b68484cbdda5b4
jormaraty <jorma.raty@metropolia.fi>
2026-01-19 11:25:24 +0200

    Eka versio.

---
+++ mod-03/Teht4.py
@@ -0,0 +1,20 @@
+"""
+Onko annettu vuosi karkausvuosi?
+
+Algoritmi 1:
+JOS vuosi on jaollinen 400:lla ->
+    ON karkausvuosi
+MUUTEN JOS vuosi on jaollinen 100:lla ->
+    EI ole karkausvuosi
+MUUTEN JOS vuosi on jaollinen 4:lla ->
+    ON karkausvuosi
+MUUTEN ->
+    EI ole karkausvuosi
+
+Algoritmi 2:
+JOS vuosi on jaollinen 400:lla TAI
+(vuosi EI ole jaollinen 100:lla JA vuosi ???)  ->
+    vuosi ON karkausvuosi
+MUUTEN
+    vuosi EI ole karkausvuosi
+"""
