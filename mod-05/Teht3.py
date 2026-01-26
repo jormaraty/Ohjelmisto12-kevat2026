62a540971c063774fff743c733d29fc32575fa47
jormaraty <jorma.raty@metropolia.fi>
2026-01-26 15:08:23 +0200

    Karkea hahmotelma

--- mod-05/Teht3.py
+++ mod-05/Teht3.py
@@ -9,9 +9,18 @@ tutkittava = int( input("Anna tutkittava kokonaisluku: "))
 # jakaja, jolla tutkitaan käyttäjän luvun jaollisuutta.
 jakaja = 2
 
-# Idea: jos käyttäjän luku on jaollinen 2:lla.
-# Seuraavaksi jaan 3:lla, 4, 5, ... (kayttajan luku - 1)
-# Jos löytyy yksikin jakajan arvo, jolla käyttäjän luku on jaollinen,
-# niin käyttäjän luku EI ole alkuluku. Merkitään on_alkuluku = False.
-# Huom: toistorakenteen voi lopettaa heti, jos jakolasku menee tasan.
+# Idea: tutkitaan, että onko käyttäjän antama luku jaollinen
+# jollakin jakajan arvolla 2, 3, 4, 5, ... (kayttajan luku - 1).
+# Jos löytyy jakajan arvo, jolla käyttäjän luku on jaollinen,
+# niin muutetaan muuttujan on_jaollinen arvo (on_jaollinen = False).
+
+# Jos löytyi yksikin jakajan arvo, jolla käyttäjän luku on jaollinen,
+# niin muuttujan 'on_alkuluku' on muuttunut arvoon False.
+# Tämä tarkoittaa ettei käyttäjän luku ole alkuluku.
+
+# Jos muuttujan 'on_alkuluku' on säilynyt arvossa True kaikilla jakajan
+# eri arvoilla, niin se tarkoittaa ettei käyttäjän antama luku ole
+# jaollinen millään muulla kuin ykkösellä ja itsellään.
+# Toisin sanoen käyttäjän antama luku ON alkuluku.
+
 
