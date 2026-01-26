6f8625195f4935a09c98c1b2f1ebe913ea32a441
jormaraty <jorma.raty@metropolia.fi>
2026-01-26 11:10:59 +0200

    Hahmotelma

---
+++ mod-05/Teht2.py
@@ -0,0 +1,10 @@
+# Tehdään tyhjä lista, johon käyttäjän antamat luvut tallennetaan.
+
+# Kysytään käyttäjältä lukuja, kunnes hän antaa tyhjän merkkijonon.
+# Ahaa, käyttäjän syöte luettava merkkijonona, muuten ei havaita lopetusmerkkiä.
+
+# lajitellaan listan alkiot käänteiseen suuruusjärjestykseen
+# (suurimmasta pienimpään).
+
+# Tulostetaan järjestetystä listasta 5 ekaa alkiota.
+
