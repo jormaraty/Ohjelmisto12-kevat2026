3949dfe553545ed3ad5893ba94ef089984f2b5eb
jormaraty <jorma.raty@metropolia.fi>
2026-02-02 10:00:17 +0200

    Hahmotelma.

---
+++ mod-07/Teht1.py
@@ -0,0 +1,8 @@
+# Tehdään monikko, joka sisältää eri vuodeanaikojen nimet,
+# 4-alkioinen monikko, joka sisältää arvot "Kevät", "Kesä, "Syksy", "Talvi"
+
+# Kysytään käyttäjältä kuukauden numero
+
+# Kuukauden numeron perusteella valitaan monikosta oikea arvo, ja
+# tulostetaan vastaus
+
