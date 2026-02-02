56b9b64b282cf9d217d75fe26f05e77db8f2f66f
jormaraty <jorma.raty@metropolia.fi>
2026-02-02 11:08:18 +0200

    Karkea hahmotelma.

--- mod-07/Teht3.py
+++ mod-07/Teht3.py
@@ -2,7 +2,7 @@
 
 def lisaa():
     # funktio lisää uuden lentokentän sanakirjaan
-    pass
+    print("Toiminto ei ihan vielä valmis...")
 
 def hae():
     # funktio hakee lentokentän tiedot
@@ -25,9 +25,12 @@ lentoasemat = {
 toiminto = 0    # käyttäjän valitsema toiminnon nro
 
 while toiminto != 9:
+    tulosta_valikko()
+    toiminto = int(input("Valitse toiminto: "))
+
     if toiminto == 1:
         lisaa()
-    # tässä välissä on muut toiminnot (elif)
+    # tässä välissä on muut toiminnot (elif -haarat)
     else:
         print("Tuntematon toiminto")
 
