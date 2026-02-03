bc05d882f013433c38b79a498136d5004d78fa38
ratyj <jorma.raty@metropolia.fi>
2026-02-03 18:51:32 +0200

    Toka versio.

--- mod-07/Esim1.py
+++ mod-07/Esim1.py
@@ -1,11 +1,18 @@
 import random
 
 def heitä():
+    # arvotaan 2 satunnaista kokonaislukua (int)
     eka, toka = random.randint(1,6), random.randint(1,6)
+
+    # palautetaan 1 kpl monikkoja, joka sisältää 2 arvoa
     return (eka, toka)
 
+# Tapa 1: otetaan funktion palauttama monikko (tuple) talteen monikkona
+vastaus_monikko = heitä()
+# puretaan monikon sisältämät 2 arvoa.
+print(f"Nopista tuli {vastaus_monikko[0]} ja {vastaus_monikko[1]}")
+
+# Tapa 2: sijoitetaan funktion palauttaman monikon 2 arvoa suoraan 2 eri muuttujaan.
 noppa1, noppa2 = heitä()
 print(f"Nopista tuli {noppa1} ja {noppa2}.")
 
-vastaus_monikko = heitä()
-print(f"Nopista tuli {vastaus_monikko[0]} ja {vastaus_monikko[1]}")
