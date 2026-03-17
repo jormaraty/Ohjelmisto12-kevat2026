77f552abccb625ee8a4e5e2d4b9fd6f488e84d6a
ratyj <jorma.raty@metropolia.fi>
2026-03-17 14:28:26 +0200

    Siistitty versio.

--- mod-09/Esim1.py
+++ mod-09/Esim1.py
@@ -1,10 +1,12 @@
 """
+Tehtävä:
 Koodaa Kissa-luokka.
-Kissa luokalla on 2 ominaisuutta: nimi ja rotu.
-Jos käyttäjä ei anna kissalle rodun nimeä, niin se arvoksi
+Kissa-luokalla on 2 ominaisuutta: nimi ja rotu.
+Jos käyttäjä ei anna kissalle rodun nimeä, niin sen arvoksi
 "kotikissa".
-Tee luokalle alustaja, jossa voidaan antaa luotavan olion
-arvot.
+Tee luokalle alustaja, jossa voidaan antaa luotavan
+Kissa -tyyppisen olion kaikki ominaisuudet.
+
 Luo pääohjelmassa 2 kissaa: eka kissan rotuna on 'Ragdoll',
 toiselle ei määritellä rotua. Keksi itse kissojen nimet.
 Tulosta molempien kissojen kaikki ominaisuudet.
@@ -14,8 +16,10 @@ class Kissa:
 
     def __init__(self, nimi, laji="kotikissa"):
         self.nimi = nimi
-        # huom: parametrin nimi on eri luokan ominaisuus.
+        # huom: parametrin nimi on eri kuin luokan ominaisuus.
+        # Olion ominaisuuden nimeksi tulee 'rotu'.
         self.rotu = laji
+
 
 # pääohjelma alkaa
 
@@ -23,7 +27,7 @@ class Kissa:
 kissa1 = Kissa("Miska", "Ragdoll")
 kissa2 = Kissa("Minni")
 
-# tulostetaan molempien kissojenkaikki arvot
+# tulostetaan molempien kissojen kaikki arvot
 print(f"Nimi: {kissa1.nimi} on {kissa1.rotu}")
 print(f"{kissa2.nimi} on {kissa2.rotu}")
 
