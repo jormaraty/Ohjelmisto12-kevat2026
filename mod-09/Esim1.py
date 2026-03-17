be95c34ad86ca6b95fcbee8c4e9dc44cddd0696d
jormaraty <jorma.raty@metropolia.fi>
2026-03-17 11:13:48 +0200

    Eka versio.

--- mod-09/Esim1.py
+++ mod-09/Esim1.py
@@ -10,4 +10,20 @@ toiselle ei määritellä rotua. Keksi itse kissojen nimet.
 Tulosta molempien kissojen kaikki ominaisuudet.
 """
 
+class Kissa:
+
+    def __init__(self, nimi, laji="kotikissa"):
+        self.nimi = nimi
+        # huom: parametrin nimi on eri luokan ominaisuus.
+        self.rotu = laji
+
+# pääohjelma alkaa
+
+# luodaan kaksi kissa-tyyppistä oliota
+kissa1 = Kissa("Miska", "Ragdoll")
+kissa2 = Kissa("Minni")
+
+# tulostetaan molempien kissojenkaikki arvot
+print(f"Nimi: {kissa1.nimi} on {kissa1.rotu}")
+print(f"{kissa2.nimi} on {kissa2.rotu}")
 
