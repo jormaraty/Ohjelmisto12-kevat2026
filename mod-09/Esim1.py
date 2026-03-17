5fb6ef9cc7a29094bdd4b8f49826bc27bae26808
jormaraty <jorma.raty@metropolia.fi>
2026-03-17 10:55:03 +0200

    Tehtävän määrittely.

---
+++ mod-09/Esim1.py
@@ -0,0 +1,13 @@
+"""
+Koodaa Kissa-luokka.
+Kissa luokalla on 2 ominaisuutta: nimi ja rotu.
+Jos käyttäjä ei anna kissalle rodun nimeä, niin se arvoksi
+"kotikissa".
+Tee luokalle alustaja, jossa voidaan antaa luotavan olion
+arvot.
+Luo pääohjelmassa 2 kissaa: eka kissan rotuna on 'Ragdoll',
+toiselle ei määritellä rotua. Keksi itse kissojen nimet.
+Tulosta molempien kissojen kaikki ominaisuudet.
+"""
+
+
