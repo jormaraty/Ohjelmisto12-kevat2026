8bc149071cf479c81104cbe36d5e20d78d5d5bf2
jormaraty <jorma.raty@metropolia.fi>
2026-03-24 09:11:51 +0200

    Työversio.

---
+++ mod-11/Polkupyörä.py
@@ -0,0 +1,20 @@
+"""
+Tässä on esimerkki olio-ohjelmoinnin periytymisestä.
+
+Polkupyörä-luokalla on ominaisuudet polkupyörän merkki ja vaihteiden lukumäärä.
+Tee luokalle konstruktori, jossa voit asettaa nämä arvot.
+Tee luokalle myös metodi tulosta(), joka tulostaa polkupyörän kaikki tiedot.
+
+Tee myös luokka Sähköpyörä, joka periytyy Polkypyörä-luokasta.
+Sähköpolkupyörä-luokalla on lisäksi ominaisuudet omistaja sekä toimintasäde sähköllä.
+Hyödynnä aliluokassa (Sähköpolupyörä) sen yliluokan (Polupyörä) valmiita koodeja.
+
+Tee pääohjelma, jossa luot polkypyörän ja tulostat sen kaikki ominaisuudet.
+Tee lisäksi sähköpolupyörä ja tulosta sen kaikki ominaisuudet.
+"""
+
+class Polkupyörä:
+    def __init__(self):
+        pass
+
+    # TODO: täydennä luokasta puuttuvat koodit
