478510b6b72c6434c8dc3ef8617b18d3229d2792
jormaraty <jorma.raty@metropolia.fi>
2026-03-31 10:24:46 +0300

    Työversio.

---
+++ mod-12/Testi1.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+hakusana = input("Anna hakusana: ")
+
+# Pyynnön malli: https://api.tvmaze.com/search/shows?q=girls
+
+# muodostetaan nettiin lähetettävä pyyntö
+pyyntö = "https://api.tvmaze.com/search/shows?q=" + hakusana
+print("Nettiin lähtevä pyyntö: " + pyyntö)
+
+try:
+    # lähetetään nettiin GET-pyyntö, saadaan takaisin json muotoinen data
+    vastaus = requests.get(pyyntö).json()
+    print(vastaus)
+
+    # vastauksen muotoilua
+    # print(json.dumps(vastaus, indent=2))
+
+    # tulostetaan löytyneiden sarjojen nimet
+    for a in vastaus:
+        print(a["show"]["name"])
+
+except requests.exceptions.RequestException as e:
+    # käsitellään nettihaussa syntyneet poikkeukset
+    print ("Hakua ei voitu suorittaa.")
+
+# voinko käsitellä myös muut poikkeukset?
+
+
+
+
+
+
